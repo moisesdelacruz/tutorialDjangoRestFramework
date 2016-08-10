997bc21dd71b3390ad17b5fc23c6226cc32bf0f2
moisesdelacruz <moises-jesus_15@hotmail.com>
2016-08-10 01:53:07 -0400

    Part Five Finished. Using Routers.

--- snippets/views.py
+++ snippets/views.py
@@ -2,25 +2,15 @@
 from django.contrib.auth.models import User
 # from rest_framework
 from rest_framework.decorators import detail_route
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 from rest_framework import renderers
 from rest_framework import permissions
-from rest_framework import generics
 from rest_framework import viewsets
 # from snippets
 from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
 from snippets.models import Snippet
 
-# View Root
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
-    })
 
 # Views Snippet
 class SnippetViewSet(viewsets.ModelViewSet):
